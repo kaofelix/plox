66e70d1aedeecdbf99a6c3eb1848bf3d3c53cf03
Kao Félix <me@kaofelix.dev>
2026-01-23 01:23:54 +0100

    add expression ast classes with ast printer

---
+++ src/plox/expressions.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+from asyncio import Protocol
+from dataclasses import dataclass
+from functools import singledispatch
+
+from plox.scanner import Token, TokenType
+
+
+class Expr(ABC):
+    pass
+
+
+@dataclass
+class Binary(Expr):
+    left: Expr
+    operator: Token
+    right: Expr
+
+
+@dataclass
+class Grouping(Expr):
+    expression: Expr
+
+
+@dataclass
+class Literal(Expr):
+    value: object
+
+
+@dataclass
+class Unary(Expr):
+    operator: Token
+    right: Expr
+
+
+@singledispatch
+def ast_printer(expr: Binary):
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+
+@ast_printer.register
+def _(expr: Grouping):
+    return parenthesize("group", expr)
+
+
+@ast_printer.register
+def _(expr: Literal):
+    return str(expr.value)
+
+
+@ast_printer.register
+def _(expr: Unary):
+    return parenthesize(expr.operator.lexeme, expr.right)
+
+
+def parenthesize(name: str, *exprs: Expr):
+    return f"({name} {' '.join(map(ast_printer, exprs))})"
+
+
+if __name__ == "__main__":
+    a = Literal(value=123)
+    plus = Token(TokenType.PLUS, lexeme="+", literal="+", line=1)
+    b = Literal(value=432)
+    print(ast_printer(Binary(left=a, operator=plus, right=b)))
